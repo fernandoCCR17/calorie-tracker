b3fe8e0119ac5726876badcb71a59b7e73e33c2d
Fernando de la Cruz Ramos <fernan654happy@gmail.com>
2025-08-31 12:59:11 -0600

    first commit

---
+++ src/reducers/activity-reducer.ts
@@ -0,0 +1,71 @@
+import type { Activity } from "../types"
+
+//Solo sirve para definir el cuerpo que tendrá la variable
+export type ActivityActions = 
+    {type: 'save-activity', payload: { newActivity: Activity}} |
+    {type: 'set-activeId', payload: { id: Activity['id'] }} |
+    {type: 'delete-activity', payload: { id: Activity['id'] }} |
+    {type: 'restart-app'} 
+
+//Solo sirve para definir el cuerpo que tendrá la variable
+export type ActivityState = {
+    activities: Activity[],
+    activeId: Activity['id']
+}
+
+const localStorageActivities = (): Activity[] => {
+    const activities = localStorage.getItem('activities');
+    return activities ? JSON.parse(activities) : [];
+}
+
+export const initialState: ActivityState = {
+    activities: localStorageActivities(),
+    activeId: ''
+}
+
+export const activityReducer = (
+    state: ActivityState = initialState,
+    action: ActivityActions
+) => {
+    if(action.type === 'save-activity'){
+        //Este código maneja la lógica para actualizar el state
+        let updatedActivities: Activity[] = []
+
+        if(state.activeId){
+            updatedActivities = state.activities.map(activity => 
+                activity.id === state.activeId ? action.payload.newActivity : activity
+            );
+        }else{
+            updatedActivities = [...state.activities, action.payload.newActivity]
+        }
+
+        return {
+            ...state,
+            activities: updatedActivities,
+            activeId: ''
+        }
+    }
+
+    if(action.type === 'set-activeId'){
+        return {
+            ...state,
+            activeId: action.payload.id
+        }
+    }
+
+    if(action.type === 'delete-activity'){
+        return{
+            ...state,
+            activities: state.activities.filter(activity => activity.id !== action.payload.id)
+        }
+    }
+
+    if(action.type === 'restart-app'){
+        return{
+            activities: [],
+            activeId: ''
+        }
+    }
+
+    return state;
+}
